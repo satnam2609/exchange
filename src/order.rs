e340a6f0e44aff406fbb32e19a74bdc80fe2fadd
satnam2609 <satnamm143@gmail.com>
2025-09-04 19:14:57 +0530

    Seq_id type change and added memmap crate based raw order for parsing

--- src/order.rs
+++ src/order.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 use serde::{Deserialize, Serialize};
+use memmap::engseq::RawSequencedOrder;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize,Deserialize)]
 pub enum Side {
@@ -19,7 +20,7 @@ pub enum OrderType {
 }
 #[derive(Debug, Clone, Serialize,Deserialize)]
 pub struct RawOrder {
-    pub seq_id: String,
+    pub seq_id: u128,
     pub order_id: String,
     pub quote: String,
     pub price: f64,
@@ -28,9 +29,27 @@ pub struct RawOrder {
     pub order_type: OrderType,
 }
 
+
+impl From<RawSequencedOrder> for RawOrder{
+    fn from(value: RawSequencedOrder) -> Self {
+        let side= if value.side {Side::BID} else {Side::ASK};
+        let order_type= if value.order_type { OrderType::LIMIT} else {OrderType::MARKET};
+
+        Self{
+            seq_id:value.seq_id,
+            order_id:value.order_id,
+            quote:value.quote,
+            price:value.price,
+            size:value.size,
+            side,
+            order_type,
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct Order {
-    pub seq_id: String,
+    pub seq_id: u128,
     pub order_id: String,
     pub quote: String,
     pub price: f64,
@@ -70,3 +89,4 @@ impl From<RawOrder> for Order {
         }
     }
 }
+
